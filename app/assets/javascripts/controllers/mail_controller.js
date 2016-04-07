0a97dec77236efee8ff70de249321ecaa8081d95
Kit Langton <kitlangton@gmail.com>
2016-04-07 13:30:32 -0400

    layout mail page

--- app/assets/javascripts/controllers/mail_controller.js
+++ app/assets/javascripts/controllers/mail_controller.js
@@ -1,4 +1,11 @@
 angular.module('mailNurse')
 .controller('MailCtrl', ['$scope', '$mdDialog', '$window', function($scope, $mdDialog, $window) {
-
+  $scope.emails = [];
+  for (var i = 0; i < 15; i++) {
+    $scope.emails.push({
+      subject: "Brunch this weekend?",
+      from: "Min Li Chan",
+      preview: "I'll be in your neighborhood doing errands."
+    });
+  }
 }]);
