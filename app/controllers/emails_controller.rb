a82ca292b19b2700b9df5ef4a70b1011508d682f
Kit Langton <kitlangton@gmail.com>
2016-04-08 17:00:25 -0400

    create

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,4 +3,14 @@ class EmailsController < ApplicationController
     @emails = GmailAPI.new(current_user).grab_all
     render json: @emails
   end
+
+  def create
+    @emails = GmailAPI.new(current_user).send(email_params)
+  end
+
+  private
+
+  def email_params
+    params.require(:email).permit(:to, :subject, :body)
+  end
 end
